807e82e1ac33c1c0190b24a95e8bd51f880fe2e8
Ali Ahsan Noon <alinoonciit@gmail.com>
2019-01-20 03:51:32 +0500

    Creating CategoryController

---
+++ Shop.Web/Models/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Web.Models
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryService categoryService = new CategoryService();
+        public ActionResult Index()
+        {
+            var model = categoryService.GetCategories();
+            return View(model);
+        }
+    }
+}
